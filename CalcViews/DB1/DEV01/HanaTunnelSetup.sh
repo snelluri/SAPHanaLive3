eb0d351b5607b46f8247408c21980ec1e18e476c
snelluri <sivaram.nelluri@gmail.com>
2018-10-14 19:12:46 -0700

    Create HanaTunnelSetup.sh

---
+++ CalcViews/DB1/DEV01/HanaTunnelSetup.sh
@@ -0,0 +1,5 @@
+#Setup a tunnel between hana trial and local using the following:
+bash neo.sh open-db-tunnel -h hanatrial.ondemand.com -u P760686 -a p760686trial --id sivarealtime
+
+#Before this step make sure neo sdk setup is downloaded and cd to tools folder. 
+
